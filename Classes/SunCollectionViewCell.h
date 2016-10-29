7bb4d0f8420e4e79bdc858fcbb9a5d748502dd10
sunbohong <sunbohong@gmail.com>
2016-10-29 20:36:39 +0800

    bump to 0.1.0

--- Classes/SunCollectionViewCell.h
+++ Classes/SunCollectionViewCell.h
@@ -24,8 +24,26 @@
 
 @interface SunCollectionViewCell : UICollectionViewCell
 
-@property (nonatomic, strong, readonly) UIImageView *imageView;
-@property (nonatomic, strong, readonly) UILabel *textLabel;
-@property (nonatomic, strong, readonly) UILabel *detailTextLabel;
+@property (nonatomic, assign) BOOL needConfig;
+
+@property (nonatomic, readonly) UIImageView *imageView;
+@property (nonatomic, readonly) UILabel *textLabel;
+@property (nonatomic, readonly) UILabel *detailTextLabel;
+@property (nonatomic, readonly) CALayer *separator;
+
+/**
+   allows customization of the separator color.
+   This only affect appearance,if you want change separator'color,you should set cell.separator.backgroundColor
+ */
+@property (nonatomic, strong) UIColor *separatorColor UI_APPEARANCE_SELECTOR;
+
+/**
+   allows customization of the separator frame
+ */
+@property (nonatomic) UIEdgeInsets separatorInset UI_APPEARANCE_SELECTOR;
+
+
+@property (nonatomic, copy) void (^didMoveToSuperviewBlock)(SunCollectionViewCell *cell);
+@property (nonatomic, copy) void (^didLayoutSubviewsBlock)(SunCollectionViewCell *cell);
 
 @end
--- Classes/SunCollectionViewCell.m
+++ Classes/SunCollectionViewCell.m
@@ -12,6 +12,40 @@
     UIImageView *_imageView;
     UILabel *_textLabel;
     UILabel *_detailTextLabel;
+    CALayer *_separator;
+    BOOL _useSeparatorInset;
+}
+
++ (void)load {
+    SunCollectionViewCell *cell = [SunCollectionViewCell appearance];
+    cell.separatorColor = [UIColor colorWithRed:200/255.0 green:199/255.0 blue:204/255.0 alpha:1];
+    cell.separatorInset = UIEdgeInsetsMake(0, 15, 0, 0);
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if(self = [super initWithFrame:frame]) {
+        _needConfig = YES;
+    }
+    return self;
+}
+
+#pragma mark - set
+- (void)setSeparatorInset:(UIEdgeInsets)separatorInset {
+    _separatorInset    = separatorInset;
+    _useSeparatorInset = YES;
+}
+
+#pragma mark - view
+
+- (CALayer *)separator {
+    if(!_separator) {
+        _separator = [CALayer new];
+        UIColor *backgroundColor = self.separatorColor ? :[SunCollectionViewCell appearance].separatorColor;
+
+        _separator.backgroundColor = backgroundColor.CGColor;
+        [self.contentView.layer addSublayer:_separator];
+    }
+    return _separator;
 }
 
 - (UIImageView *)imageView {
@@ -36,6 +70,30 @@
         [self.contentView addSubview:_detailTextLabel];
     }
     return _detailTextLabel;
+}
+
+- (void)didMoveToSuperview {
+    [super didMoveToSuperview];
+    !self.didMoveToSuperviewBlock ? : self.didMoveToSuperviewBlock(self);
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    if(_separator) {
+        UIEdgeInsets inset = self.separatorInset;
+        if(!_useSeparatorInset) {
+            inset = [SunCollectionViewCell appearance].separatorInset;
+        }
+
+        CGRect separatorFrame = CGRectZero;
+        separatorFrame.origin.x    = inset.left;
+        separatorFrame.origin.y    = self.contentView.frame.size.height-0.5-inset.bottom;
+        separatorFrame.size.height = 0.5;
+        separatorFrame.size.width  = ceil( self.contentView.frame.size.width-inset.left-inset.right);
+        _separator.frame = separatorFrame;
+    }
+
+    !self.didLayoutSubviewsBlock ? : self.didLayoutSubviewsBlock(self);
 }
 
 @end
